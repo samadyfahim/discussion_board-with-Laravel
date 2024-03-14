60538e975e55a7da9df753d6a77e47cd9a64344d
fahim <fahim.samady2001@gmail.com>
2024-03-14 18:05:30 +0000

    add tables for post and comments and add file seeder for the user post and comment

--- database/migrations/2024_03_14_175116_create_comments_table.php
+++ database/migrations/2024_03_14_175116_create_comments_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('comments', function (Blueprint $table) {
+        SSchema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->text('content');
             $table->timestamps();
         });
     }
--- database/migrations/2024_03_14_175120_create_posts_table.php
+++ database/migrations/2024_03_14_175120_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('content');
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
